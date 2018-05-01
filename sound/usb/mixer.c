344d4394eaff168f870f8361a5f538bc8ffb8673
arunassain <arun.assain98@gmail.com>
2018-05-01 13:13:49 +0000

    Fix Conflict

--- sound/usb/mixer.c
+++ sound/usb/mixer.c
@@ -2526,7 +2526,7 @@ _error:
 	return err;
 }
 
-void snd_usb_mixer_disconnect(struct list_head *p)
+void snd_usb_mixer_disconnect(struct usb_mixer_interface *mixer)
 {
 	struct usb_mixer_interface *mixer;
 
